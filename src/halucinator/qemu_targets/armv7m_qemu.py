a321810baecebe81f3da8777d4b1ed626f63b6be
Ming Lee <ming@lee.com>
2020-12-08 18:20:44 +1030

    interrupts working; still a lil confused

--- src/halucinator/qemu_targets/armv7m_qemu.py
+++ src/halucinator/qemu_targets/armv7m_qemu.py
@@ -1,19 +1,17 @@
 from .arm_qemu import ARMQemuTarget
+from .. import hal_log
+hal_log = hal_log.getHalLogger()
 
 class ARMv7mQemuTarget(ARMQemuTarget):
 
     def trigger_interrupt(self, interrupt_number, cpu_number=0):
+        hal_log.info("Injecting interrupt %d" % interrupt_number)
         self.protocols.monitor.execute_command(
             'avatar-armv7m-inject-irq',
             {'num_irq': interrupt_number, 'num_cpu': cpu_number})
 
     def set_vector_table_base(self, base, cpu_number=0):
+        hal_log.info("Setting vector table base to 0x%x" % base)
         self.protocols.monitor.execute_command(
             'avatar-armv7m-set-vector-table-base',
             {'base': base, 'num_cpu': cpu_number})
-
-    def enable_interrupt(self, interrupt_number, cpu_number=0):
-        self.protocols.monitor.execute_command(
-            'avatar-armv7m-enable-irq',
-            {'num_irq': interrupt_number, 'num_cpu': cpu_number})
-
