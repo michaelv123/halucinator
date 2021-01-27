e8cebbf91e6126fbbc51b2f0b731f18484594318
Ming Lee <ming@lee.com>
2021-01-27 11:47:35 +1030

    printer config done

--- test/P375DW/extract.sh
+++ test/P375DW/extract.sh
@@ -2,3 +2,4 @@
 
 dd if=D015U4_D2007201727 of=pagetable.bin bs=1 count=$((0x8400)) skip=$((0x93))
 dd if=D015U4_D2007201727 of=kernel.bin bs=1 count=$((0x49314)) skip=$((0x8827))
+dd if=D015U4_D2007201727 of=physical.bin bs=1 skip=$((0x93))
